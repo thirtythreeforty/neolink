a34eebff6dbd6423bb1255301e2b8b8b1e456a21
QuantumEntangledAndy <sheepchaan@gmail.com>
2020-09-18 13:01:57 +0700

    Install all of gstraemer for mac pkg
    I originally planned to extract the pkg components from the runtime 
    deployment pkg and use them. But they are not valid outside of the 
    bundle so we have to deploy the whole gstreamer app.

--- macos_packager/postinstall.sh
+++ macos_packager/postinstall.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-installer -pkg base-system.pkg -target "/"
-installer -pkg gstreamer-core.pkg -target "/"
-installer -pkg gstreamer-net.pkg -target "/"
-installer -pkg gstreamer-codecs.pkg -target "/"
+installer -pkg "$(pwd)/gstreamer-runtime.pkg" -target "/"
