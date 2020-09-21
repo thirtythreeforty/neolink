1b7679b9b9c42b9a1874682e66b0178e31a0ad3a
QuantumEntangledAndy <sheepchaan@gmail.com>
2020-09-21 13:53:53 +0700

    Add non overwriting config to mac pkg

--- macos_packager/postinstall.sh
+++ macos_packager/postinstall.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
 installer -pkg "$(pwd)/gstreamer-runtime.pkg" -target "/"
+
+sample_toml=/usr/local/share/neolink/sample_config.toml
+live_toml=usr/local/etc/neolink.toml
+if [ ! -e "${live_toml}" ]; then
+  cp "${sample_toml}" "${live_toml}"
+fi
