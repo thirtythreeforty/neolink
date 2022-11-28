b80144492e12fa0fe61d5515fb70cf18ba6d7bb6
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-11-28 19:35:10 +0700

    Use newer config for mqtt

--- src/mqtt/cmdline.rs
+++ src/mqtt/cmdline.rs
@@ -1,10 +1,5 @@
-use std::path::PathBuf;
 use structopt::StructOpt;
 
 /// The reboot command will reboot the camera
 #[derive(StructOpt, Debug)]
-pub struct Opt {
-    /// The path to the config file
-    #[structopt(short, long, parse(from_os_str))]
-    pub config: PathBuf,
-}
+pub struct Opt {}
