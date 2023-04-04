426f1deb2a7d64a58988a968791c99ebc4fa9af4
jl1990 <jlcarrasco1990@gmail.com>
2023-04-05 01:36:56 +0200

    Fix statusled warning

--- src/statusled/mod.rs
+++ src/statusled/mod.rs
@@ -27,7 +27,7 @@ pub(crate) use cmdline::Opt;
 ///
 /// Opt is the command line options
 pub(crate) fn main(opt: Opt, config: Config) -> Result<()> {
-    let mut camera = find_and_connect(&config, &opt.camera)?;
+    let camera = find_and_connect(&config, &opt.camera)?;
 
     camera
         .led_light_set(opt.on)
