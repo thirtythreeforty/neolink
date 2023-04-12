7faddb3b50578b5a8be69bdbeaed87128b8c7fae
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-04-12 15:57:44 +0700

    Add plugin base to docker

--- Dockerfile
+++ Dockerfile
@@ -50,6 +50,7 @@ RUN apt-get update && \
     apt-get install -y --no-install-recommends \
         libgstrtspserver-1.0-0 \
         libgstreamer1.0-0 \
+        gstreamer1.0-plugins-base \
         gstreamer1.0-plugins-good \
         gstreamer1.0-plugins-bad && \
     apt-get clean -y && rm -rf /var/lib/apt/lists/*
@@ -59,7 +60,8 @@ COPY --from=build \
   /usr/local/bin/neolink
 COPY docker/entrypoint.sh /entrypoint.sh
 
-RUN chmod +x "/usr/local/bin/neolink" && \
+RUN gst-inspect-1.0; \
+    chmod +x "/usr/local/bin/neolink" && \
     "/usr/local/bin/neolink" --version
 
 CMD ["/usr/local/bin/neolink", "rtsp", "--config", "/etc/neolink.toml"]
--- src/rtsp/gst/factory.rs
+++ src/rtsp/gst/factory.rs
@@ -46,7 +46,7 @@ impl NeoMediaFactory {
     pub(crate) fn new() -> Self {
         let factory = Object::new::<NeoMediaFactory>(&[]);
         factory.set_shared(false);
-        // factory.set_do_retransmission(false);
+        // factory.set_do_retransmission(false); // Can't use as the method is missing on the 32bit docker gst dll
         factory.set_launch("videotestsrc pattern=\"snow\" ! video/x-raw,width=896,height=512,framerate=25/1 ! textoverlay name=\"inittextoverlay\" text=\"Stream not Ready\" valignment=top halignment=left font-desc=\"Sans, 32\" ! jpegenc ! rtpjpegpay name=pay0");
         factory.set_suspend_mode(RTSPSuspendMode::None);
         factory.set_transport_mode(RTSPTransportMode::PLAY);
