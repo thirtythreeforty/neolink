37049f7ca4a6967d33ceaf67f95f39a0f2c70fac
Andrew <sheepchaan@gmail.com>
2023-04-15 09:43:08 +0700

    Fix bytes remaining on stream

--- crates/core/src/bcudp/codex.rs
+++ crates/core/src/bcudp/codex.rs
@@ -33,6 +33,15 @@ impl Decoder for BcUdpCodex {
     type Item = BcUdp;
     type Error = Error;
 
+    /// Since frames can cross EOF boundaries we overload this so it dosen't error if
+    /// there are bytes left on the stream
+    fn decode_eof(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>> {
+        match self.decode(buf)? {
+            Some(frame) => Ok(Some(frame)),
+            None => Ok(None),
+        }
+    }
+
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>> {
         match { BcUdp::deserialize(src) } {
             Ok(BcUdp::Discovery(UdpDiscovery {
