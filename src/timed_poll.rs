0b922aeeb5fbd353e68ad64ca5d51fa57807d0a8
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-05-03 13:29:47 +0700

    Always timed polls

--- src/timed_poll.rs
+++ src/timed_poll.rs
@@ -24,14 +24,14 @@ impl<F: Future> Future for TimedPoll<F> {
     type Output = F::Output;
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<F::Output> {
         let name = self.name.clone();
-        // log::debug!("{}: Start poll", name);
+        log::debug!("{}: Start poll", name);
         let before = std::time::Instant::now();
         let future = unsafe { Pin::map_unchecked_mut(self, |me| &mut me.future) };
         let res = future.poll(cx);
         let elapsed = before.elapsed();
-        if elapsed > std::time::Duration::from_micros(500) {
-            log::debug!("{}: Stop poll: {:?}", name, before.elapsed());
-        }
+        // if elapsed > std::time::Duration::from_micros(500) {
+        log::debug!("{}: Stop poll: {:?}", name, elapsed);
+        // }
         res
     }
 }
