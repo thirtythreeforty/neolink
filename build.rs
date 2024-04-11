7a920fb09a963854b8c32b40ae40eabe7f08d2b8
Andrew King <sheepchaan@gmail.com>
2024-04-11 09:51:40 +0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -49,7 +49,11 @@ fn github_ver() -> Option<String> {
 fn platform_cfg() {
     let gstreamer_dir = env::var_os("GSTREAMER_1_0_ROOT_X86_64")
         .and_then(|x| x.into_string().ok())
-        .unwrap_or_else(|| r#"C:\gstreamer\1.0\x86_64\"#.to_string());
+        .unwrap_or_else(|| {
+            env::var_os("GSTREAMER_1_0_ROOT_MSVC_X86_64")
+                .and_then(|x| x.into_string().ok())
+                .unwrap_or_else(|| r#"C:\gstreamer\1.0\x86_64\"#.to_string())
+        });
 
     println!(r"cargo:rustc-link-search=native={}\lib", gstreamer_dir);
 }
